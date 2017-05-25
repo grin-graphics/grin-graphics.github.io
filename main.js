0d15ee9f6555eb4da5fbca36d902d73ad7ccffe1
Grindustries <atomov303@gmail.com>
2017-05-26 02:22:54 +1200

    Second Commit

--- main.js
+++ main.js
@@ -2,9 +2,11 @@
 setTimeout(function () {
     $("#overlay").fadeOut();
 }, 1900);
-$( document ).ready(function() {
+$(document).ready(function () {
     $(".styleCssLink").click(function () {
-            window.location = "style.css";
-        });
-   
+        window.location = "style.css";
+    });
+    $(".mainIndexLink").click(function () {
+        window.location = "example.html";
+    });
 });
